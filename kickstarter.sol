d2fded34451188b3e13d0f06c1375f5487e33959
Jung <seologan1@gmail.com>
2022-05-31 00:17:53 -0400

    adding approveRequest function

--- kickstarter.sol
+++ kickstarter.sol
@@ -7,11 +7,14 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address = > bool) approvals;
     }
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    mapping(address => bool) public approvers;
 
     modifier restricted() {
         require(msg.sender == manager);
@@ -25,6 +28,31 @@ contract Campaign {
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+    }
+    function createRequest(string description, uint value, address recipeint) 
+        public restricted {
+            Request memory newRequest = Request({
+                description: description,
+                value: value,
+                recipient: recipient,
+                complete: false,
+                approvalCount: 0;
+            });
+
+            
+
+            requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+
+        
     }
 }
